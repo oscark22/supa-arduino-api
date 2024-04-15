bbd80692369aff5d1ae286ce28f2fd063d44a369
Oscar Montoya <98506040+oscark22@users.noreply.github.com>
2024-04-15 13:14:17 -0700

    esp route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const supabaseClient = require("@supabase/supabase-js");
+const morgan = require("morgan");
 const bodyParser = require("body-parser");
 
 require("dotenv").config();
 const app = express();
+
+app.use(morgan("combined"));
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -13,20 +16,54 @@ const supabase = supabaseClient.createClient(
   (supabaseKey = process.env.SUPA_ARDUINO_API_KEY)
 );
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
 
-app.post("/mkr/upload", async (req, res) => {
-  const { data, error } = await supabase.from("mkr1000").insert({
-    humidity: req.body.humidity,
-    temperature: req.body.temperature,
-  });
+app.get("/mkr", async (req, res) => {
+  const { data, error } = await supabase
+    .from("mkr")
+    .select()
+    .limit(10)
+    .order("timestamp", { ascending: false });
   if (error) {
-    res.send(error);
+    res.send("an error has occurred.");
   } else {
     res.send(data);
   }
 });
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.post("/mkr", async (req, res) => {
+  const { data, error } = await supabase
+    .from("mkr")
+    .insert([
+      {
+        humidity: req.body.humidity,
+        temperature: req.body.temperature,
+      },
+    ])
+    .select();
 
-module.exports = app;
+  if (error) {
+    res.send("an error has occurred. " + error);
+  } else {
+    res.send(data);
+  }
+});
+
+app.post("/esp", async (req, res) => {
+  const { data, error } = await supabase
+    .from("esp")
+    .insert([{ pressure: req.body.pressure }])
+    .select();
+
+  if (error) {
+    res.send("an error has occurred. " + error);
+  } else {
+    res.send(data);
+  }
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000...");
+});
